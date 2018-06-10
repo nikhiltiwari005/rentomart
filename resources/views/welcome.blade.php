f0a4098872abc06fe8dc9cba8309c7f1b05c0b4f
nikhiltiwari005 <nikhiltiwari005@gmail.com>
2018-06-10 10:29:58 +0530

    Retomart

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    RentoMart
                 </div>
 
                 <div class="links">
